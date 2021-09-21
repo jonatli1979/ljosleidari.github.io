b2976cd196c99800c5a4c155cf91e23af8557a0e
jonatli79 <62761112+jonatli1979@users.noreply.github.com>
2021-09-21 15:31:50 +0000

    AutoUpdate

--- gitrun.py
+++ gitrun.py
@@ -6,16 +6,16 @@ Created on Thu Jul  8 07:40:51 2021
 """
 
 import subprocess as cmd
-import time
+from time import sleep
 from datetime import datetime
 
-
-   
-print('Last start of process',datetime.now())
-try:
-    cp = cmd.run("git add .", check=True, shell = True)
-    cmd.run("git commit -m AutoUpdate", check=True, shell = True)
-    cmd.run("git push -u origin main", check=True, shell = True)
-    cmd.run("cd ..", check=True, shell = True)
-except:
-   print('error')
+while True:
+    print('Last start of process',datetime.now())
+    try:
+        cp = cmd.run("git add .", check=True, shell = True)
+        cmd.run("git commit -m AutoUpdate", check=True, shell = True)
+        cmd.run("git push -u origin main", check=True, shell = True)
+        cmd.run("cd ..", check=True, shell = True)
+    except:
+        print('error')
+    sleep(60)
