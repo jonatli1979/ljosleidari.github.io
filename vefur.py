afeca7f3e0e69f24b7b153b66dc4fdf2e355badf
jonatli79 <62761112+jonatli1979@users.noreply.github.com>
2021-09-28 14:18:34 +0000

    Update vefur.py

--- vefur.py
+++ vefur.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return psycopg2.connect(database = "Natthagi", user="jonatli",password="CallMore",host="192.168.1.167",port="5432")
+    return psycopg2.connect(database = "Natthagi", user=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password,host="192.168.1.167",port="5432")
 conn = init_connection()
 
 
